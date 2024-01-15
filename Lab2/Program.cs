3860bb3456fdeac97aca183d773b6321b08b6959
vaporiiise <derick6266@gmail.com>
2024-01-15 12:28:16 +0800

    added selections and did multiplication and division

--- Lab2/Program.cs
+++ Lab2/Program.cs
@@ -1,20 +1,46 @@
-﻿namespace ConsoleApp65
+﻿using System.ComponentModel.Design;
+using System.Net.NetworkInformation;
+
+namespace ConsoleApp65
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Enter Number 1: ");
-            int x = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Enter Number 2: ");
-            int y = Convert.ToInt32(Console.ReadLine());
-                        Console.WriteLine("Enter Number 4: ");
-            Console.WriteLine("enternumber");
-
-        }
-        class Multiply
-        {
+            Console.WriteLine("Menu driven Calculator");
+            Console.WriteLine("***********************");
+            Console.WriteLine("1.Addition");
+            Console.WriteLine("2.Subtraction");
+            Console.WriteLine("3.Multiplication");
+            Console.WriteLine("4.Division");
+            Console.WriteLine("***********************");
+            Console.WriteLine("type in the desired number to use the calculator's function: ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            if (a == 1)
             
+            else if (a == 2)
+            
+            else if (a == 3)
+            {
+                Console.WriteLine("You have chosen Multiplication.");
+                Console.WriteLine("Please enter Number 1: ");
+                int x = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter Number 2: ");
+                int y = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(x + " x " + y + " = " + (x * y));
+            }
+            else if (a == 4)
+            {
+                Console.WriteLine("You have chosen Division.");
+                Console.WriteLine("Please enter Number 1: ");
+                int x = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("Please enter Number 2: ");
+                int y = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine(x + " / " + y + " = " + (x / y));
+            }                        
+            else
+                Console.WriteLine("Unknown function! Please run me again.");
+
         }
     }
 }
