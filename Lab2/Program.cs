262fd3f9447013efefe0cb021e04f2ad51d45bdd
User <User@VAPOSLAPTOP>
2024-01-15 12:00:27 +0800

    Add project files.

---
+++ Lab2/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace ConsoleApp65
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter Number 1: ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter Number 2: ");
+            int y = Convert.ToInt32(Console.ReadLine());
+        }
+        class Multiply
+        {
+
+        }
+    }
+}
